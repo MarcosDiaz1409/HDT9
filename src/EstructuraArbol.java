04e049ec19055134075f95ec26c1c51146290ec5
MarcosDiaz1409 <dia221102@uvg.edu.gt>
2023-04-17 11:19:55 -0600

    Clase Palabra, ComparadorPalabras y Clase abstracta

---
+++ src/EstructuraArbol.java
@@ -0,0 +1,23 @@
+public abstract class EstructuraArbol<T>{
+
+    public void add(T value){
+        
+    }
+
+    public T get(){
+        return null;
+    }
+
+    public T remove(){
+        return null;
+    }
+
+    public int count(){
+        return 0;
+    }
+
+    public boolean isEmpty(){
+        return true;
+    }
+
+}
